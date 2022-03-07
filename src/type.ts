7a086963b7eeba05d64f27003ef36c4ce02f08bc
Mark Van houtert <mvan-hou@f1r6s10.codam.nl>
2022-03-07 14:07:06 +0100

    isAlpha

--- src/type.ts
+++ src/type.ts
@@ -4,6 +4,8 @@ export const isUpper = (c: string): boolean => c >= 'A' && c <= 'Z';
 
 export const isLower = (c: string): boolean => c >= 'a' && c <= 'z';
 
+export const isAlpha = (c: string): boolean => isUpper(c) || isLower(c);
+
 export const isSpace = (c: string): boolean => [' ', '\n', '\t', '\r', '\v'].includes(c);
 
 export const int = (c: string): number => c.charCodeAt(0);

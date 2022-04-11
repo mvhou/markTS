f35d5c704428e50074642e7e1de5570cb7fffa8c
Mark van Hou <mvhou@Marks-MacBook-Air.local>
2022-04-11 21:32:32 +0200

    minor fixes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * as T from './type';
 
 export * as A from './array';
+
+export * as S from './string'
--- src/string.ts
+++ src/string.ts
@@ -2,4 +2,4 @@ export const capitalize = (str:string) => str.slice(0,1).toUpperCase() + str.sli
 
 export const padFront = (str:string, width:number, char:string=' ') => (new Array(Math.max(width - str.length, 0))).fill(char).join('') + str
 
-export const padBack = (str:string, width:number, char:string=' ') => str + (new Array(Math.max(width - str.length))).fill(char).join('')
+export const padBack = (str:string, width:number, char:string=' ') => str + (new Array(Math.max(width - str.length, 0))).fill(char).join('')
